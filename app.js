259fdc7b11b297fd2ef7a2e80379f203bdf0a750
Abhishek Maheshwari <mr.ab1992@gmail.com>
2017-10-28 20:19:07 +0530

    update 2
    update 1

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+// tee shirt lai ke manange
 app.use('/', index);
 app.use('/users', users);
 
